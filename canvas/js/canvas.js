05c73b9dd83747d19492dc33049bb562995960f7
fucaobao <466368084@qq.com>
2015-11-12 23:28:11 +0800

    增加gulp学习demo

---
+++ canvas/js/canvas.js
@@ -0,0 +1,402 @@
+// var eventType = isPC() ? "click" : "tap";
+var eventType = "click";
+//上传图片相关的参数
+var params = {
+    "upload": "http://qwd.jd.com/cgi-bin/image_upload",
+    "div_height": 50, //底部"取消"和"选取"所在div的高度
+    "timeout": 20000,
+    "sqrt": 1,
+    "isScale": false //图片是否有放大/缩小过
+};
+//获取手机屏幕宽高
+var c_w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+    c_h = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) - params.div_height;
+var can_obj = document.querySelector("#canvas"),
+    ctx_img = can_obj.getContext("2d");
+var headpic = {
+    init: function() {
+        //发布的时候需要删除该调试文件 --- BEGIN
+        // loadscript("http://10.9.248.120:8080/target/target-script-min.js#anonymous");
+        //发布的时候需要删除该调试文件 --- END
+        var me = this;
+        me.setCanvasXY();
+        me.bindEvent();
+    },
+    setCanvasXY: function() {
+        //设置canvas 宽度（全屏显示），高度,上下居中显示
+        can_obj.width = c_w - 2;
+        can_obj.height = c_w - 2;
+        can_obj.style.top = (c_h - c_w - 2) / 2 + "px";
+    },
+    bindEvent: function() {
+        $(".file").on("change", fileChange);
+    },
+    handleUpload: function(file, callback) {
+        var imageReader = new FileReader();
+        imageReader.readAsDataURL(file);
+        imageReader.onload = function() {
+            var el = document.getElementById("canvas_img");
+            //图片加载完成之后再执行回调函数
+            el.onload = function() {
+                el.onload = null; //避免重复触发
+                EXIF.getData(el, function() {
+                    var allTag = EXIF.getAllTags(el);
+                    var mpImg = new MegaPixImage(file);
+                    var tmpCanvas = document.createElement("canvas"),
+                        tmpContext = tmpCanvas.getContext("2d");
+                    tmpContext.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height); //清除画布
+                    var w = allTag.PixelXDimension,
+                        h = allTag.PixelYDimension;
+                    if (w >= 3264 || h >= 2448) {
+                        w = (w * 0.5).toFixed(2);
+                        h = (h * 0.5).toFixed(2);
+                    }
+                    mpImg.render(tmpCanvas, {
+                        maxWidth: w || c_w,
+                        maxHeight: h || c_h,
+                        quality: 1,
+                        orientation: allTag.Orientation || 1
+                    });
+                    setTimeout(function() {
+                        var base64 = tmpCanvas.toDataURL('image/jpeg', 1);
+                        tmpContext = null;
+                        tmpCanvas = null;
+                        $("#canvas_img").attr("src", base64);
+                        callback && callback(file, allTag);
+                    }, 300); //等待前面的canvas绘制完成后再执行
+                });
+            };
+            el.src = this.result;
+        };
+    },
+    uploadModule: function(file, allTag) {
+        var div_obj = document.querySelector("#canvas_div"),
+            img_obj = document.querySelector("#canvas_img");
+        var orientation = allTag.Orientation || 1;
+        var posX = 0,
+            posY = 0; //相对坐标
+        var scale = 0; //记录在缩放动作执行前的 缩放值
+        var start_X1 = 0,
+            start_Y1 = 0,
+            start_X2 = 0,
+            start_Y2 = 0;
+        var start_sqrt = 0, //开始缩放比例
+            sqrt = params.sqrt;
+        var left_x = 0,
+            left_y = 0; //计算 偏移量 设置画布中的X，Y轴 (加偏移量)
+        var distX = 0,
+            distY = 0; //最后绘制canvas时候的X和Y值
+        load();
+
+        function load() {
+            img_obj.onload = function() {
+                $("#loading span").text("正在加载图片...");
+                $("#loading").hide();
+                $("#handleImage").show();
+                //在图片显示后，设置图片自适应大小及图片的居中显示
+                _autoResizeImage(allTag);
+                img_obj.style.top = (c_h - img_obj.height - 2) / 2 + "px";
+                img_obj.style.left = (c_w - img_obj.width) / 2 + "px";
+                distX = (can_obj.width - img_obj.width) / 2;
+                distY = (can_obj.height - img_obj.height) / 2;
+            };
+            div_obj.addEventListener('touchstart', touch, false);
+            div_obj.addEventListener('touchmove', touch, false);
+            div_obj.addEventListener('touchend', touchend, false);
+            // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+            function touchend(event) {
+                event.preventDefault(); //阻止浏览器或body 其他冒泡事件
+                if (!params.isScale && sqrt <= params.sqrt) {
+                    sqrt = params.sqrt;
+                    img_obj.style.webkitTransform = "scale(" + sqrt + ")"; //设置放大缩小
+                    img_obj.style.Transform = "scale(" + sqrt + ")";
+                    img_obj.style.left = "0px";
+                    //图片离顶部的距离
+                    var imgTopX = parseFloat(img_obj.style.top);
+                    //canvas框离顶部的距离
+                    var canTopX = parseFloat(can_obj.style.top);
+                    //图片的高度
+                    var imgHeight = img_obj.height;
+                    //canvas框的高度
+                    var canHeight = can_obj.height;
+                    //如果图片比canvas框小，则始终居中显示
+                    if (img_obj.height < can_obj.height) {
+                        img_obj.style.top = (can_obj.height - img_obj.height) / 2 + canTopX + "px";
+                    } else {
+                        if (imgTopX > canTopX) {
+                            img_obj.style.top = canTopX + "px";
+                        }
+                        if (imgTopX + imgHeight < canTopX + canHeight) {
+                            img_obj.style.top = canTopX + canHeight - imgHeight + 1 + "px";
+                        }
+                    }
+                    var _x = 0;
+                    var _y = img_obj.offsetTop;
+                    distX = _x + left_x / 2 - 1;
+                    distY = _y - parseFloat(can_obj.style.top) + left_y / 2 - 1;
+                    // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+                } else {
+                    if (sqrt <= params.sqrt) { //如果是缩小
+                        sqrt = params.sqrt;
+                        img_obj.style.webkitTransform = "scale(" + sqrt + ")"; //设置放大缩小
+                        img_obj.style.Transform = "scale(" + sqrt + ")";
+                        img_obj.style.left = "0px";
+                        var _x = 0;
+                        var _y = img_obj.offsetTop;
+                        left_x = 0;
+                        left_y = 0;
+                        distX = _x + left_x / 2 + h - 1;
+                        distY = _y - parseFloat(can_obj.style.top) + left_y / 2 - 1;
+                        // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+                        params.isScale = false;
+                    } else { // 如果是放大
+                        var dImg_left = parseFloat(img_obj.style.left),
+                            dImg_top = parseFloat(img_obj.style.top),
+                            dCav_top = parseFloat(can_obj.style.top);
+                        var w = img_obj.width,
+                            h = img_obj.height,
+                            sw = w * sqrt,
+                            sh = h * sqrt;
+                        left_x = w - sw; //计算 偏移量 设置画布中的X，Y轴 (加偏移量) 注：canvas 原点放大（canvas中图片左上角坐标），css3 scale 中点放大
+                        left_y = h - sh;
+                        var left_img = dImg_left + left_x / 2 - 1,
+                            top_img = dImg_top + left_y / 2 - dCav_top - 1;
+                        //如果左侧超出范围
+                        if (left_img > 0) {
+                            left_img = 0;
+                            img_obj.style.left = Math.abs(left_x / 2) + "px";
+                        }
+                        //如果上面超出范围
+                        if (top_img > 0) {
+                            img_obj.style.top = Math.abs(left_y / 2) + dCav_top + "px";
+                            top_img = 0;
+                        }
+                        //如果右侧超出范围
+                        if (left_img + sw < can_obj.width) {
+                            left_img = left_x;
+                            img_obj.style.left = left_x / 2 + "px";
+                        }
+                        //如果下面超出范围
+                        if (top_img + sh < can_obj.height) {
+                            img_obj.style.top = (c_h + can_obj.height - (sqrt + 1) * h) / 2 + "px";
+                            top_img = can_obj.height - sh;
+                        }
+                        distX = left_img;
+                        distY = top_img;
+                        // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+                        params.isScale = false;
+                    }
+                }
+            }
+
+            function touch(event) {
+                event.preventDefault(); //阻止浏览器或body 其他冒泡事件
+                var touchList = event.touches;
+                var mv_x1 = touchList[0].clientX,
+                    mv_y1 = touchList[0].clientY; //手指坐标
+                var img_left = img_obj.left,
+                    img_top = img_obj.top; //图片坐标
+                if (touchList.length == 1) { //单指操作
+                    if (event.type == "touchstart") { //开始移动
+                        posX = mv_x1 - img_obj.offsetLeft; //获取img相对坐标
+                        posY = mv_y1 - img_obj.offsetTop;
+                        // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+                    } else if (event.type == "touchmove") { //移动中
+                        // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+                        var _x = mv_x1 - posX; //移动坐标
+                        var _y = mv_y1 - posY;
+                        img_obj.style.left = _x + "px";
+                        img_obj.style.top = _y + "px";
+                    }
+                } else if (touchList.length == 2) { //双指操作
+                    if (event.type == "touchstart") {
+                        scale = img_obj.style.Transform == undefined ? 1 : parseFloat(img_obj.style.Transform.replace(/[^0-9^\.]/g, "")); //获取在手指按下瞬间的放大缩小值（scale），作用，在移动时，记录上次移动的放大缩小值
+                        start_X1 = touchList[0].clientX; //记录开始的坐标值,作用：在下次放大缩小后，去掉上次放大或缩小的值
+                        start_Y1 = touchList[0].clientY;
+                        start_X2 = touchList[1].clientX;
+                        start_Y2 = touchList[1].clientY;
+                        start_sqrt = Math.sqrt(Math.pow(start_X2 - start_X1, 2) + Math.pow(start_Y2 - start_Y1, 2)) / 200; //获取在缩放时 当前缩放的值
+                        // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+                    } else if (event.type == "touchmove") {
+                        // ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+                        params.isScale = true;
+                        var mv_x2 = touchList[1].clientX,
+                            mv_y2 = touchList[1].clientY;
+                        var move_sqrt = Math.sqrt(Math.pow(mv_x2 - mv_x1, 2) + Math.pow(mv_y2 - mv_y1, 2)) / 200; //动态获取上一次缩放值(随时变更)，在下次缩放时减去上一次的值，作用：防止累加之前的缩放
+                        sqrt = move_sqrt - start_sqrt + scale; //求出缩放值
+                        img_obj.style.webkitTransform = "scale(" + sqrt + ")"; //设置放大缩小
+                        img_obj.style.Transform = "scale(" + sqrt + ")";
+                    }
+                }
+            }
+        }
+        //裁图
+        $("#save_img").on(eventType, function() {
+            ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+            ctx_img.drawImage(img_obj, distX, distY, img_obj.width * sqrt, img_obj.height * sqrt);
+            var base64 = can_obj.toDataURL('image/png', 1);
+            $("#loading").show();
+            uploadImg(file, base64, function(url) {
+                $(".headpic_preview img").attr("src", url);
+            });
+            _initImg();
+            $("#save_img").off(eventType); //修复多次执行回调函数的BUG
+        });
+        $("#cancel_img").on(eventType, function() {
+            _initImg();
+            $("#cancel_img").off(eventType); //修复多次执行回调函数的BUG
+        });
+
+        function _initImg() {
+            fixUploadImgBug();
+            params.isScale = false;
+            sqrt = params.sqrt;
+            ctx_img.clearRect(0, 0, can_obj.width, can_obj.height); //清除画布
+            //删掉图片的长度和宽度，以免对下一次求图片长度/宽度的时候发生错误
+            $("#canvas_img").removeAttr("height").removeAttr("width").removeAttr("style");
+            var tpl = $("#handleImage").html();
+            //先清空再添加进去，是为了消除影响
+            $("#handleImage").html("").html(tpl).hide();
+        }
+        //图片宽度始终与屏幕宽度相等
+        function _autoResizeImage(allTag) {
+            var w = allTag.PixelXDimension || img_obj.width;
+            var h = allTag.PixelYDimension || img_obj.height;
+            // 超过这个值base64无法生成，在IOS上
+            if (w >= 3264 || h >= 2448) {
+                w = (w * 0.5).toFixed(2);
+                h = (h * 0.5).toFixed(2);
+            }
+            //如果向左或者向右旋转了
+            if (orientation <= 8 && orientation >= 5) {
+                img_obj.width = c_w;
+                img_obj.height = c_w * w / h;
+            } else {
+                img_obj.width = c_w;
+                img_obj.height = c_w * h / w;
+            }
+        }
+    }
+};
+
+function uploadImg(file, base64, callback) {
+    $("#loading").show();
+    var formData = new FormData();
+    //兼容安卓的文件名可能是路径的问题
+    formData.append("imageurl", _getFileName(file.name));
+    formData.append("base64", 1);
+    // formData.append("rx", 78); //宽
+    // formData.append("ry", 78); //高
+    //删掉前面的"data:image/png;base64,"
+    formData.append("content", base64.substring(22));
+    var xhr = new XMLHttpRequest();
+    xhr.open("post", params.upload, true);
+    xhr.send(formData);
+    xhr.onreadystatechange = function(resp) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            try {
+                var respData = resp.currentTarget.response,
+                    data = JSON.parse(respData);
+                if (data.errCode == 0) {
+                    var url = _changeImageUrl(decodeURIComponent(data.imageurl));
+                    typeof callback === "function" && callback(url);
+                } else {
+                    alert("上传图片失败，请稍候再试！");
+                }
+            } catch (e) {
+                alert("上传图片失败，请稍候再试！");
+            } finally {
+                xhr.abort(); //这里将readyState置为0
+                $("#loading").hide();
+            }
+        }
+    };
+    setTimeout(function() {
+        if (xhr.readyState !== 0) {
+            xhr.abort();
+            alert("上传图片失败，请稍候再试！");
+        }
+        $("#loading").hide();
+    }, params.timeout);
+
+    function _getFileName(name) {
+        var index = name.lastIndexOf("/");
+        if (index === -1) {
+            return name;
+        }
+        return name.substring(index + 1);
+    }
+
+    function _changeImageUrl(url, width, height) {
+        var index = url.indexOf("/jfs/");
+        width = width || 200;
+        height = height || 200;
+        return url.substring(0, index + 1) + "s" + width + "x" + height + "_" + url.substring(index + 1);
+    }
+}
+// 判断图片是否加载完成
+function loadImage(url, callback) {
+    var image = new Image();
+    if (image.readyState) { //IE
+        image.onreadystatechange = function() {
+            if (image.readyState == "complete" || image.readyState == "loaded") {
+                image.onreadystatechange = null;
+                typeof callback === "function" && callback(url);
+            }
+        };
+    } else { //others
+        image.onload = function() {
+            image.onload = null;
+            typeof callback === "function" && callback(url);
+        };
+    }
+    image.onerror = function() {
+        console.log("load image failed!");
+    };
+    image.src = url;
+}
+//修复连续选择同一张图片后无反应的BUG
+//主要思路，先删除该节点，然后插入一个一模一样的节点，然后再绑定一次
+function fixUploadImgBug() {
+    $(".file").off("change").remove();
+    $("#upload").prepend('<input class="file" type="file" name="imageurl" accept="image/*">');
+    $(".file").on("change", fileChange);
+}
+
+function fileChange() {
+    var file = this.files[0];
+    $("#loading span").text("正在加载图片...");
+    $("#loading").show();
+    headpic.handleUpload(file, headpic.uploadModule);
+}
+//调试用 --- BEGIN
+function loadscript(url, callback, charset) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    charset && (script.charset = charset);
+    if (script.readyState) { //IE
+        script.onreadystatechange = function() {
+            if (script.readyState == 'loaded' || script.readyState == 'complete') {
+                script.onreadystatechange = null;
+                typeof callback === "function" && callback();
+            }
+        };
+    } else { //others
+        script.onload = function() {
+            script.onload = null;
+            typeof callback === "function" && callback();
+        };
+    }
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+//判断是不是PC
+function isPC() {
+    var userAgentInfo = navigator.userAgent,
+        Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    return Agents.every(function(item, index) {
+        return userAgentInfo.indexOf(item) === -1;
+    });
+}
+//调试用 --- END
+headpic.init();
---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+var gulp = require('gulp');
+var	gulpSequence = require('gulp-sequence');//按顺序执行
+var uglify = require('gulp-uglify');	   	//压缩JS
+var minifyCss = require('gulp-minify-css');	//压缩CSS
+var imagemin = require('gulp-imagemin');   	//压缩图片
+var concat = require('gulp-concat');       	//合并文件
+var jshint = require('gulp-jshint');       	//合并文件
+var map = require('map-stream');
+var del = require('del');				   	//删除文件
+var rename = require('gulp-rename');	   	//修改文件名
+var rev = require('gulp-rev');             	//MD5
+var fs = require('fs');             		//文件操作
+var mkdirp = require('mkdirp');             //递归新建文件夹
+var paths = {
+    js: ['./canvas/js/*.js'],
+    css: ['./canvas/css/*.css'],
+    images: ['./canvas/images/*.*'],
+    page: ['./canvas/*.html'],
+    build: 'build'
+};
+var getTimestamp = function(){
+	var dt = new Date();
+        y = dt.getFullYear(),
+        M = dt.getMonth() + 1,
+        d = dt.getDate(),
+        h = dt.getHours(),
+        m = dt.getMinutes(),
+        sec = dt.getSeconds(),
+        minsec = dt.getMilliseconds();
+        while (String(minsec).length < 3) {
+            minsec = "0" + minsec;
+        }
+        return String(y) + _addPrefix(M) + _addPrefix(d) + _addPrefix(h) + _addPrefix(m) + _addPrefix(sec) + minsec;
+
+    	function _addPrefix(num) {
+            return num < 10 ? '0' + num : num;
+        }
+};
+var getFileName = function(path){
+    var suffix = '.txt',
+        prefix = '/report-';
+    if(!path){
+        return prefix + getTimestamp() + suffix;
+    }
+    var indexL = path.lastIndexOf('/'),
+        indexR = path.lastIndexOf('\\');
+    if(indexL === -1 && indexR === -1){
+        return prefix + getTimestamp() + suffix;
+    } else if(indexL !== -1){
+        return prefix + path.substring(indexL + 1) + suffix;
+    } else {
+        return prefix + path.substring(indexR + 1) + suffix;
+    }
+};
+gulp.task('clean', function (callback) {
+    del(paths.build, callback);
+});
+
+var myReporter = map(function(file, cb) {
+	var parent = paths.build + '/report/';
+    mkdirp(parent, function(err, callback) {
+        if (!file.jshint.success) {
+            var filename = parent + getFileName(file.path);
+            file.jshint.results.forEach(function(err) {
+                if (err) {
+                    fs.appendFile(filename, 'file:'+ err.file + ', line ' + err.error.line + ', col ' + err.error.character + ', code ' + err.error.code + ', ' + err.error.reason + '\n');
+                }
+            });
+        }
+        cb(null, file);
+    });
+});
+gulp.task('jshint', function () {
+    return gulp.src(paths.js)
+        .pipe(jshint())
+        .pipe(myReporter);
+        // .pipe(jshint.reporter('default'));
+});
+gulp.task('js', function () {
+    return gulp.src(paths.js)
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.build + '/js'));
+});
+gulp.task('css', function () {
+    return gulp.src(paths.css)
+        .pipe(minifyCss())
+        .pipe(gulp.dest(paths.build + '/css'));
+});
+gulp.task('images', function () {
+    return gulp.src(paths.images)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.build + '/images'));
+});
+gulp.task('page', function () {
+    return gulp.src(paths.page)
+        .pipe(gulp.dest(paths.build));
+});
+gulp.task('watch', function() {
+    gulp.watch(paths.js, ['js']);
+    gulp.watch(paths.css, ['css']);
+    gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.page, ['page']);
+});
+gulp.task('default', ['js', 'css', 'images', 'page', 'jshint']);
+// gulp.task('default', gulpSequence('clean', ['js', 'css', 'images', 'page']));
+gulp.task('test1', function(callback){
+    console.log(1);//执行了
+    // gulp.run('build');
+    // console.log(2)
+});
+gulp.task('test2',['clean'], function(callback){
+    console.log(2);//没有执行
+    // gulp.run('build');
+});
